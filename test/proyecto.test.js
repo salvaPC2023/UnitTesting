60fb7580da836cc70d67972126088add7d5e8156
RosaR20 <rosareynaga2025@gmail.com>
2025-04-09 15:29:27 -0400

    T4 aniadir commit

--- test/proyecto.test.js
+++ test/proyecto.test.js
@@ -15,9 +15,26 @@ describe('Proyecto Class Tests', function() {
     
             
      it('debería asignar correctamente el título', () => {
-            // Comprobamos si el título se asigna correctamente
+            
             expect(proyecto.getTitulo()).to.equal('Mi Proyecto');
         });
+
+    
+    it("debería añadir un commit correctamente", () => {
+        proyecto.aniadirCommit(3, 100, 80, "regular");
+        
+        const commits = proyecto.mostrarCommits();
+        
+        expect(commits[0]).to.deep.include({
+            cantPruebas: 3,
+            cantLineas: 100,
+            cobertura: 80,
+            complejidad: "regular",
+            recomendacion: "Se recomienda mejorar la cantidad de pruebas aprobadas. ",
+            frecuencia: "Regular"
+        });
+     });
+          
 
 
   });
